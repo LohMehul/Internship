946c9d7edc3539dcf78892808d27af836cd88d5b
Loh Mehul <115700236+LohMehul@users.noreply.github.com>
2023-12-29 09:57:41 +0530

    data

--- setup/src/components/Layout.jsx
+++ setup/src/components/Layout.jsx
@@ -19,7 +19,7 @@ const App = () => {
       >
         <div className="demo-logo" />
         <Link to="/" className=""><i className="fa-solid fa-house nav-logo"></i></Link>
-        <Link to="/login" className='font-color'> login</Link>&nbsp;&nbsp;
+        <Link to="/login" className='font-color'> Login</Link>&nbsp;&nbsp;
         <Link to="/register" className='font-color'> SignUp</Link>
       </Header>     
     </Layout>
